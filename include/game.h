c43db26570bf579bacbcef8fedda9b1b909fe05a
Paul <paul.doenicke@online.de>
2025-09-22 18:46:51 +0200

    board.h

--- include/game.h
+++ include/game.h
@@ -1,12 +1,9 @@
-//
-// Created by pauld on 18.09.2025.
-//
-
 #ifndef SCHACH_IN_C_GAME_H
 #define SCHACH_IN_C_GAME_H
 
 bool isGameOver();
 void getCommand();
 void changePlayer();
+bool getPlayerToMove();
 
 #endif //SCHACH_IN_C_GAME_H
